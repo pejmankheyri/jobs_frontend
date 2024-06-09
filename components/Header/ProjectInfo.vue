0dbc79695ab680b2a135f93fe6bd8c3881a7bce7
pejmankheyri <pejmankheyri@gmail.com>
2024-06-09 23:36:06 +0200

    add: redis

--- components/Header/ProjectInfo.vue
+++ components/Header/ProjectInfo.vue
@@ -23,7 +23,7 @@ const isOpen = ref(false);
               {{ $t("PROJECT_TECHNICAL_DETAILS") }}
             </h3>
             <UButton
-              color="gray"
+              color="indigo"
               variant="ghost"
               icon="i-heroicons-x-mark-20-solid"
               class="-my-1"
