93c195b0bc33938aa4aeb84363ff7cf549b726fa
pejmankheyri <pejmankheyri@gmail.com>
2025-03-30 13:44:18 +0200

    refactor: enhance dashboard layout with new styles and responsive design

--- components/dashboard/DashboardIndex.vue
+++ components/dashboard/DashboardIndex.vue
@@ -1,14 +1,11 @@
 <script setup>
 import { ref, onMounted, computed } from "vue";
 const route = useRoute();
-const { t, localePath } = useI18n();
+const { t } = useI18n();
 
-const config = useRuntimeConfig();
-
-// Reactive jobs data
 const jobs = ref([]);
 const error = ref("");
-const page = ref(1); // Track the current page
+const page = ref(1);
 const loading = ref(false);
 const selectedJob = ref(null);
 const itemsPerPage = 7;
@@ -17,7 +14,6 @@ const displayedJobs = computed(() => {
   return jobs.value;
 });
 
-// Fetch jobs data from API
 const fetchJobs = async () => {
   if (loading.value) return;
   try {
@@ -41,7 +37,6 @@ const fetchJobs = async () => {
   }
 };
 
-// watch route params and fetch jobs data
 watch(
   () => route.params,
   () => {
@@ -56,11 +51,9 @@ onMounted(fetchJobs);
 </script>
 
 <template>
-  <div
-    class="container grid items-center justify-center grid-cols-1 gap-4 pt-8 align-middle sm:grid-cols-2 lg:grid-cols-3 place-items-center"
-  >
-    <div v-if="displayedJobs.length" class="h-full job-list scrollbar-hide">
-      <div>
+  <div v-if="displayedJobs.length" class="dashboard-container">
+    <div class="jobs-column scrollbar-hide">
+      <div class="job-list scrollbar-hide">
         <JobsList
           v-for="job in displayedJobs"
           :key="job.id"
@@ -84,39 +77,17 @@ onMounted(fetchJobs);
         </div>
       </div>
     </div>
-    <USkeleton v-else-if="loading" class="h-[100px] w-full mb-auto mt-16" />
-    <p v-else-if="error">{{ error }}</p>
-    <p v-else>{{ $t("NO_JOBS_FOUND") }}</p>
-    <div
-      class="relative hidden col-span-2 mb-auto rounded-md job-details md:block"
-      :class="{ 'border-black border dark:border-gray-400': selectedJob }"
-    >
-      <JobDetails :selectedJob="selectedJob" />
+
+    <div class="details-column">
+      <div
+        class="rounded-md job-details"
+        :class="{ 'border-black border dark:border-gray-400': selectedJob }"
+      >
+        <JobDetails :selectedJob="selectedJob" />
+      </div>
     </div>
   </div>
+  <USkeleton v-else-if="loading" class="h-[100px] w-full mb-auto mt-8" />
+  <p v-else-if="error">{{ error }}</p>
+  <p v-else>{{ $t("NO_JOBS_FOUND") }}</p>
 </template>
-
-<style scoped>
-.container {
-  height: 100vh; /* Set container height to viewport height */
-  display: flex;
-}
-.job-list {
-  flex: 0 0 380px; /* Fixed width for the job list */
-  overflow-y: auto; /* Allow job list to scroll if content overflows */
-}
-
-.job-details {
-  flex: 1; /* Take remaining space */
-  overflow-y: auto; /* Allow job details to scroll if content overflows */
-}
-
-.scrollbar-hide::-webkit-scrollbar {
-  display: none;
-}
-
-.scrollbar-hide {
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-}
-</style>
